166c85732bd2f44e689eb97f93a5516a5d57e5df
Raul <raul.alhena@gmail.com>
2024-02-20 20:49:46 +0100

    relationship done

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Post } from 'src/posts/entities/post.entity';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +11,7 @@ export class User {
 
   @Column({ unique: true })
   email: string;
+
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
 }
