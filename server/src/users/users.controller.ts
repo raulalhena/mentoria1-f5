7ae0be213a2b6c387f2b5ae880da79251b31492b
Raul <raul.alhena@gmail.com>
2024-02-20 19:09:02 +0100

    posts entity created successfully

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -6,7 +6,4 @@ import { UpdateUserDto } from './dto/update-user.dto';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  
-  
 }
